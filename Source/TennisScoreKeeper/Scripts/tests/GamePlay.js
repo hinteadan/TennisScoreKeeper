4357bac1ca4112a2cf9db7e5f7c053306d818834
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2013-08-05 16:16:18 +0300

    Begin tests for Gameplay

---
+++ Source/TennisScoreKeeper/Scripts/tests/GamePlay.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    "use strict";
+
+    var game;
+
+    module("Game Play", {
+        setup: function () {
+            game = {
+                points: []
+            };
+        }
+    });
+
+    test("Scoring a point is recorded", function () {
+        scorePoint();
+        ok(game.points.length === 1);
+    });
+
+    function scorePoint() {
+        game.points.push({});
+    }
+
+}).call(this);
