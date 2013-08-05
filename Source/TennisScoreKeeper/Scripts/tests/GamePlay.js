15e6162f109f997535801f93ad033f905c7bc621
Hintea Dan Alexandru <hinteadan@gmail.com>
2013-08-05 23:33:49 +0300

    Tests: Scoring a point mandatory stuff

--- Source/TennisScoreKeeper/Scripts/tests/GamePlay.js
+++ Source/TennisScoreKeeper/Scripts/tests/GamePlay.js
@@ -1,7 +1,8 @@
-﻿(function () {
+﻿(function (undefined) {
     "use strict";
 
-    var game;
+    var game,
+        player = {};
 
     module("Game Play", {
         setup: function () {
@@ -16,14 +17,31 @@
         ok(game.points.length === 1);
     });
 
-    test("Scoring a point is recorded per user", function () {
-        var player = {};
+    test("Scoring a point is recorded per player", function () {
         scorePointFor(player);
         ok(game.points[0].player === player);
     });
 
+    test("Scoring a point mandatory stuff", function () {
+        scorePointFor(player);
+        ok(notEmpty(game.points[0].creditedTo), "creditedTo");
+        ok(notEmpty(game.points[0].timestamp), "timestamp");
+        ok(notEmpty(game.points[0].type), "type");
+    });
+
     function scorePointFor(player) {
-        game.points.push({ player: player });
+        game.points.push({
+            player: player,
+            creditedTo: player,
+            timestamp: new Date(),
+            type: {}
+        });
+    }
+
+    function notEmpty(field) {
+        return field != undefined
+            && field != null
+        ;
     }
 
 }).call(this);
