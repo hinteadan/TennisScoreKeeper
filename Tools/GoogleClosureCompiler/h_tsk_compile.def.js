3775544e61b435c9e51f9a5b95614f9e80336363
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2013-08-12 11:55:11 +0300

    Closure compiler stuff

---
+++ Tools/GoogleClosureCompiler/h_tsk_compile.def.js
@@ -0,0 +1,12 @@
+// ==ClosureCompiler==
+// @compilation_level SIMPLE_OPTIMIZATIONS
+// @output_file_name h_tsk.js
+// @use_closure_library true
+// @code_url https://raw.github.com/hinteadan/TennisScoreKeeper/master/Source/TennisScoreKeeper/Scripts/Lib/lodash.js
+// @code_url https://raw.github.com/hinteadan/TennisScoreKeeper/master/Source/TennisScoreKeeper/Scripts/Lib/string.js
+// @code_url https://raw.github.com/hinteadan/TennisScoreKeeper/master/Source/TennisScoreKeeper/Scripts/namespace.js
+// @code_url https://raw.github.com/hinteadan/TennisScoreKeeper/master/Source/TennisScoreKeeper/Scripts/Lib/check.js
+// @code_url https://raw.github.com/hinteadan/TennisScoreKeeper/master/Source/TennisScoreKeeper/Scripts/Model/DataModel.js
+// @code_url https://raw.github.com/hinteadan/TennisScoreKeeper/master/Source/TennisScoreKeeper/Scripts/Engine/ScoreProjector.js
+// @code_url https://raw.github.com/hinteadan/TennisScoreKeeper/master/Source/TennisScoreKeeper/Scripts/Engine/GameEngine.js
+// ==/ClosureCompiler==
