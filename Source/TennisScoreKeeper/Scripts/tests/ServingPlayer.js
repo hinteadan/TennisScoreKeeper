f6a88731fbd88c7e55413e1c51c7c340a1339901
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2013-08-12 17:19:09 +0300

    Test for tiebreak serving player

--- Source/TennisScoreKeeper/Scripts/tests/ServingPlayer.js
+++ Source/TennisScoreKeeper/Scripts/tests/ServingPlayer.js
@@ -25,11 +25,56 @@
         ok(gameEngine.tennisScore().ServingPlayer() === fed);
     });
 
+    test("Change of serve with tie breaks", function () {
+        ok(gameEngine.tennisScore().ServingPlayer() === fed);
+        winOnePerfectSetFor(fed, 6);
+        winOnePerfectSetFor(rafa, 6);
+        ok(gameEngine.tennisScore().ServingPlayer() === fed);
+        
+        winOnePerfectGameFor(fed);
+        winOnePerfectGameFor(fed);
+        winOnePerfectGameFor(fed);
+        winOnePerfectGameFor(fed);
+        winOnePerfectGameFor(fed);
+
+        winOnePerfectGameFor(rafa);
+        winOnePerfectGameFor(rafa);
+        winOnePerfectGameFor(rafa);
+        winOnePerfectGameFor(rafa);
+        winOnePerfectGameFor(rafa);
+
+        winOnePerfectGameFor(fed);
+        ok(gameEngine.tennisScore().ServingPlayer() === rafa);
+        winOnePerfectGameFor(rafa);
+
+        ok(gameEngine.tennisScore().ServingPlayer() === fed);
+
+        //Tiebreak start
+        gameEngine.scorePointFor(fed, m.PointTypes.Ace(m.ShotStyles.NormalPassing));
+        ok(gameEngine.tennisScore().ServingPlayer() === rafa);
+        gameEngine.scorePointFor(fed, m.PointTypes.Ace(m.ShotStyles.NormalPassing));
+        gameEngine.scorePointFor(fed, m.PointTypes.Ace(m.ShotStyles.NormalPassing));
+        ok(gameEngine.tennisScore().ServingPlayer() === fed);
+        gameEngine.scorePointFor(fed, m.PointTypes.Ace(m.ShotStyles.NormalPassing));
+        gameEngine.scorePointFor(fed, m.PointTypes.Ace(m.ShotStyles.NormalPassing));
+        ok(gameEngine.tennisScore().ServingPlayer() === rafa);
+        gameEngine.scorePointFor(fed, m.PointTypes.Ace(m.ShotStyles.NormalPassing));
+        gameEngine.scorePointFor(fed, m.PointTypes.Ace(m.ShotStyles.NormalPassing));
+        ok(gameEngine.tennisScore().ServingPlayer() === fed);
+
+    });
+
     function winOnePerfectGameFor(player) {
         gameEngine.scorePointFor(player, m.PointTypes.Ace(m.ShotStyles.NormalPassing));
         gameEngine.scorePointFor(player, m.PointTypes.Ace(m.ShotStyles.NormalPassing));
         gameEngine.scorePointFor(player, m.PointTypes.Ace(m.ShotStyles.NormalPassing));
         gameEngine.scorePointFor(player, m.PointTypes.Ace(m.ShotStyles.NormalPassing));
+    }
+
+    function winOnePerfectSetFor(player, gamesPerSet) {
+        for (var i = 0; i < gamesPerSet; i++) {
+            winOnePerfectGameFor(player);
+        }
     }
 
 }).call(this, this.H.Check, this.H.TennisScoreKeeper.Model, this.H.TennisScoreKeeper);
