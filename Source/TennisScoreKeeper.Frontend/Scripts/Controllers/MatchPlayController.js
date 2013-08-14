6c7962a21922fb4314f78cd8be27b98e187ca741
Hintea Dan Alexandru <hinteadan@gmail.com>
2013-08-14 21:50:35 +0300

    Partial bind scoring screen

--- Source/TennisScoreKeeper.Frontend/Scripts/Controllers/MatchPlayController.js
+++ Source/TennisScoreKeeper.Frontend/Scripts/Controllers/MatchPlayController.js
@@ -1,4 +1,4 @@
-﻿(function (tsk) {
+﻿(function (tsk, m) {
     "use strict";
 
     function MatchPlayController(scoreKeeperEngine, $scope) {
@@ -8,11 +8,17 @@
             $scope.score = scoreKeeperEngine.tennisScore();
         }
 
+        function scorePoint(player) {
+            scoreKeeperEngine.scorePointFor(player, m.PointTypes.Ace(m.ShotStyles.NormalPassing));
+            updateScore();
+        }
+
         $scope.updateScore = updateScore;
+        $scope.scorePoint = scorePoint;
 
         updateScore();
     }
 
     this.controller('MatchPlayController', ['ScoreKeeper', '$scope', MatchPlayController]);
 
-}).call(this.H.TennisScoreKeeper.Ui.Angular.AppModule, this.H.TennisScoreKeeper);
+}).call(this.H.TennisScoreKeeper.Ui.Angular.AppModule, this.H.TennisScoreKeeper, this.H.TennisScoreKeeper.Model);
