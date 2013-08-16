aa29f009e6cecbddddc3bfd09d21f0e648f0054b
Hintea Dan Alexandru <hinteadan@gmail.com>
2013-08-16 23:19:21 +0300

    OD: ShotStyle Directive

---
+++ Source/TennisScoreKeeper.Frontend/Scripts/Directives/ShotStyle.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+
+    function ShotStyle() {
+        this.restrict = 'E';
+        this.replace = true;
+        this.templateUrl = 'Views/Templates/ShotStyle.html';
+    }
+
+    this.directive('shotStyle', [function () {
+        return new ShotStyle();
+    }]);
+
+}).call(this.H.TennisScoreKeeper.Ui.Angular.AppModule);
