5e3d517108844ab87a46e46d962eb7e31901e8b2
Hintea Dan Alexandru <hinteadan@gmail.com>
2013-08-16 11:12:05 +0300

    Game tie mode bind

--- Source/TennisScoreKeeper.Frontend/Scripts/Controllers/NewMatchController.js
+++ Source/TennisScoreKeeper.Frontend/Scripts/Controllers/NewMatchController.js
@@ -1,10 +1,24 @@
-﻿(function (tsk, check) {
+﻿(function (tsk, m, check) {
     "use strict";
 
     var app = this;
 
     function NewMatchController(matchDefinition, $scope, $location) {
         ///<param name="matchDefinition" type="tsk.Model.MatchDefinition" />
+
+        var gameTieModes = {
+                AdvantageWin: new GameTieMode('Advantage Win', m.TieMode.advantageWin),
+                SinglePointWin: new GameTieMode('Single Point Win', m.TieMode.singlePointWin)
+            };
+
+        function GameTieMode(label, tskMode){
+            var self = this;
+            this.Label = label;
+            this.Mode = tskMode;
+            this.Toggle = function(){
+                toggleTieMode(self);
+            }
+        }
 
         function startMatch() {
             $location.path('/Play');
@@ -15,12 +29,22 @@
             matchDefinition.startingPlayer = matchDefinition.players[index];
         }
 
+        function toggleTieMode(mode) {
+            matchDefinition.gameTieMode = mode.Mode;
+            $scope.selectedTieMode = mode;
+        }
+
         $scope.setStartingPlayer = setStartingPlayer;
         $scope.firstToServe = 0;
         $scope.Match = matchDefinition;
+        $scope.tieModes = gameTieModes;
+        $scope.selectedTieMode = gameTieModes.AdvantageWin;
         $scope.play = startMatch;
     }
 
     this.controller('NewMatchController', ['MatchDefinition', '$scope', '$location', NewMatchController]);
 
-}).call(this.H.TennisScoreKeeper.Ui.Angular.AppModule, this.H.TennisScoreKeeper, this.H.Check);
+}).call(this.H.TennisScoreKeeper.Ui.Angular.AppModule,
+    this.H.TennisScoreKeeper,
+    this.H.TennisScoreKeeper.Model,
+    this.H.Check);
