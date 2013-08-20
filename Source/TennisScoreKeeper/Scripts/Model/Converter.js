325b1059653d84de1fc0b756c054a805ca988aa6
Hintea Dan Alexandru <hinteadan@gmail.com>
2013-08-21 02:18:39 +0300

    Match Definition converter

--- Source/TennisScoreKeeper/Scripts/Model/Converter.js
+++ Source/TennisScoreKeeper/Scripts/Model/Converter.js
@@ -1,8 +1,39 @@
-﻿(function () {
+﻿(function (_, undefined) {
     "use strict";
+
+    var m = this;
+
+    function single(collection, id) {
+        return _.find(collection, function (t) { return t.id === id; });
+    }
+
+    function JsonConverter(jsonData) {
+
+        function convertMetadata() {
+            var metaData = jsonData.Metadata,
+                matchDefinition = new m.MatchDefinition(
+                    new m.Player(metaData.players[0].name),
+                    new m.Player(metaData.players[1].name),
+                    metaData.startingPlayer.id === metaData.players[1].id
+                );
+
+            matchDefinition.gamesPerSet = metaData.gamesPerSet;
+            matchDefinition.setsCount = metaData.setsCount;
+            matchDefinition.gameTieMode = single(m.TieMode, metaData.gameTieMode.id);
+            matchDefinition.lastSetTieMode = single(m.LastSetTieMode, metaData.lastSetTieMode.id);
+
+            return matchDefinition;
+        }
+
+        this.ToMatchDefinition = convertMetadata;
+    }
 
     function Converter() {
 
+        this.Json = function (json) { return new JsonConverter(json); }
+
     }
 
-}).call(this);
+    this.Convert = new Converter();
+
+}).call(this.H.TennisScoreKeeper.Model, this._);
