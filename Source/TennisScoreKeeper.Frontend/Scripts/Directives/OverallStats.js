d8654a16cf981210ba9d4108f0c87775fcd9d3e0
Hintea Dan Alexandru <hinteadan@gmail.com>
2013-08-27 00:39:16 +0300

    Extract overall stats in directive and display stats per match/set/game

---
+++ Source/TennisScoreKeeper.Frontend/Scripts/Directives/OverallStats.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    "use strict";
+
+    function OverallStats() {
+        this.restrict = 'E';
+        this.replace = true;
+        this.templateUrl = 'Views/Templates/OverallStats.html';
+        this.scope = {
+            stats: '=',
+        };
+    }
+
+    this.directive('overallStats', [function () {
+        return new OverallStats();
+    }]);
+
+}).call(this.H.TennisScoreKeeper.Ui.Angular.AppModule);
