7b4bacf80daf08f1bfdba7787bcaa68061f43c89
Hintea Dan Alexandru <hinteadan@gmail.com>
2013-08-28 00:13:45 +0300

    Add pNotify library

--- Source/TennisScoreKeeper.Frontend/Scripts/_references.js
+++ Source/TennisScoreKeeper.Frontend/Scripts/_references.js
@@ -11,3 +11,5 @@
 /// <reference path="Directives/ShotStyle.js" />
 /// <reference path="Services/DataService.js" />
 /// <reference path="Services/MatchDefinitionCheckService.js" />
+/// <reference path="Lib/jquery.pnotify.js" />
+/// <reference path="Lib/jquery.js" />
