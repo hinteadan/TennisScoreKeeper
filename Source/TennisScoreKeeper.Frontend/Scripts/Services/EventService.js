c43c155a2c502901ab5bca289c79ae559bd4b40a
Hintea Dan Alexandru <hinteadan@gmail.com>
2013-10-09 10:53:39 +0300

    OD: Event service

--- Source/TennisScoreKeeper.Frontend/Scripts/Services/EventService.js
+++ Source/TennisScoreKeeper.Frontend/Scripts/Services/EventService.js
@@ -1,17 +1,30 @@
-﻿(function (tsk, ui, undefined) {
+﻿(function (tsk, ui, check, undefined) {
     'use strict';
 
-    function EventService() {
+    function EventData(metadata, points) {
+        check.notEmpty(metadata, 'metadata');
+        check.notEmpty(points, 'points');
+
+        this.Metadata = metadata;
+        this.Points = points;
+    }
+
+    function EventService(matchDefinition, scoreKeeper) {
+        ///<param name="matchDefinition" type="tsk.Model.MatchDefinition" />
+        ///<param name="scoreKeeper" type="tsk.Engine" />
         
         function broadcastScoreChange() {
+            var payload = new EventData(matchDefinition, scoreKeeper.points);
             console.log('Broadcast Score Changed');
+            console.log(payload);
         }
 
         this.ScoreChanged = broadcastScoreChange;
     }
 
-    this.service('EventService', [EventService]);
+    this.service('EventService', ['MatchDefinition', 'ScoreKeeper', EventService]);
 
     ui.EventService = EventService;
 
-}).call(this.H.TennisScoreKeeper.Ui.Angular.AppModule, this.H.TennisScoreKeeper, this.H.TennisScoreKeeper.Ui);
+}).call(this.H.TennisScoreKeeper.Ui.Angular.AppModule,
+    this.H.TennisScoreKeeper, this.H.TennisScoreKeeper.Ui, this.H.Check);
